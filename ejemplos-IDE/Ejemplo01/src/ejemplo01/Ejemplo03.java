7d9c691e7c25ff70195c9d9f4293cd3ae44ccb49
Oliver Saraguro <oliversaraguro2004@gmail.com>
2022-10-26 10:51:06 -0500

    Nombres y Apellidos

---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/Ejemplo03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author SALA I
+ */
+public class Ejemplo03 {
+    public static void main(String[] args) {
+        System.out.println("Una Leyenda Cualquiera");
+    }
+          
+}
